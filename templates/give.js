85b43c687409b85e9a3e0757e6d73213d6a290b4
Hiep Nguyen <v.hiepnh7@vinid.net>
2020-05-24 23:08:55 +0700

    update template

--- templates/give.js
+++ templates/give.js
@@ -3,56 +3,14 @@ const giveTemplate = {
   callback_id: 'modal-cb',
   title: {
     type: 'plain_text',
-    text: 'Bimbim',
+    text: 'Bimbim'
   },
   submit: {
     type: 'plain_text',
     text: 'Submit',
-    emoji: true,
+    emoji: true
   },
   blocks: [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: 'Bạn muốn gửi tặng cho ai?',
-      },
-      accessory: {
-        type: 'multi_static_select',
-        placeholder: {
-          type: 'plain_text',
-          text: 'Chọn người bạn muốn tặng.',
-          emoji: true,
-        },
-        options: [
-          {
-            text: {
-              type: 'plain_text',
-              text: 'Choice 1',
-              emoji: true,
-            },
-            value: 'value-0',
-          },
-          {
-            text: {
-              type: 'plain_text',
-              text: 'Choice 2',
-              emoji: true,
-            },
-            value: 'value-1',
-          },
-          {
-            text: {
-              type: 'plain_text',
-              text: 'Choice 3',
-              emoji: true,
-            },
-            value: 'value-2',
-          },
-        ],
-      },
-    },
-
     {
       type: 'input',
       element: {
@@ -60,15 +18,31 @@ const giveTemplate = {
         action_id: 'title',
         placeholder: {
           type: 'plain_text',
-          text: 'Số bimbim muốn gửi tặng.',
+          text: 'Số bimbim muốn gửi tặng.'
         },
       },
       label: {
         type: 'plain_text',
-        text: 'Số lượng.',
-      },
+        text: 'Số lượng.'
+      }
     },
-  ],
+
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: 'Bạn muốn gửi tặng cho ai?'
+      },
+      accessory: {
+        type: 'users_select',
+        placeholder: {
+          type: 'plain_text',
+          text: 'Chọn người bạn muốn tặng.',
+          emoji: true
+        }
+      },
+    }
+  ]
 };
 
 module.exports = giveTemplate;
