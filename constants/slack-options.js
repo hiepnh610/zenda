37fa5e696e94590d484dd6e33f55367faff988cc
Hiep Nguyen <v.hiepnh7@vinid.net>
2020-05-24 08:39:31 +0700

    remove some libs

--- constants/slack-options.js
+++ constants/slack-options.js
@@ -1,7 +1,6 @@
 const SLACK_APP_OPTIONS = {
   botToken: process.env.SLACK_BOT_TOKEN,
-  clientSigningSecret: process.env.SLACK_SIGNING_SECRET,
-  scopes: ['commands,users:read.email,users:read,users.profile:read,incoming-webhook']
+  clientSigningSecret: process.env.SLACK_SIGNING_SECRET
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express     = require('express');
 const app         = express();
 const http        = require('http').Server(app);
-const axios       = require('axios');
 const bodyParser  = require('body-parser');
-const qs = require('qs');
 
 const { SlackAdapter }   = require('botbuilder-adapter-slack');
 
@@ -16,93 +14,72 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/api/messages', (req, res) => {
   slackAdapter.processActivity(req, res, async(context) => {
-    console.log('context', context);
+    console.log(context);
+    // const viewData = {
+    //   token: process.env.SLACK_BOT_TOKEN,
+    //   trigger_id: context._activity.channelData.trigger_id,
+    //   view: JSON.stringify({
+    //     "type": "modal",
+    //     "title": {
+    //       "type": "plain_text",
+    //       "text": "My App",
+    //       "emoji": true
+    //     },
+    //     "submit": {
+    //       "type": "plain_text",
+    //       "text": "Submit",
+    //       "emoji": true
+    //     },
+    //     "close": {
+    //       "type": "plain_text",
+    //       "text": "Cancel",
+    //       "emoji": true
+    //     },
+    //     "blocks": [
+    //       {
+    //         "type": "input",
+    //         "element": {
+    //           "type": "plain_text_input",
+    //           "action_id": "sl_input",
+    //           "placeholder": {
+    //             "type": "plain_text",
+    //             "text": "Placeholder text for single-line input"
+    //           }
+    //         },
+    //         "label": {
+    //           "type": "plain_text",
+    //           "text": "Label"
+    //         },
+    //         "hint": {
+    //           "type": "plain_text",
+    //           "text": "Hint text"
+    //         }
+    //       },
+    //       {
+    //         "type": "input",
+    //         "element": {
+    //           "type": "plain_text_input",
+    //           "action_id": "ml_input",
+    //           "multiline": true,
+    //           "placeholder": {
+    //             "type": "plain_text",
+    //             "text": "Placeholder text for multi-line input"
+    //           }
+    //         },
+    //         "label": {
+    //           "type": "plain_text",
+    //           "text": "Label"
+    //         },
+    //         "hint": {
+    //           "type": "plain_text",
+    //           "text": "Hint text"
+    //         }
+    //       }
+    //     ]
+    //   })
+    // };
 
-    const viewData = {
-      token: process.env.SLACK_BOT_TOKEN,
-      trigger_id: context._activity.channelData.trigger_id,
-      view: JSON.stringify({
-        type: 'modal',
-        title: {
-          type: 'plain_text',
-          text: 'Save it to ClipIt!'
-        },
-        callback_id: 'bimbim_give_modal',
-        submit: {
-          type: 'plain_text',
-          text: 'ClipIt'
-        },
-        blocks: [
-          {
-            block_id: 'message',
-            type: 'input',
-            element: {
-              action_id: 'message_id',
-              type: 'plain_text_input',
-              multiline: true,
-              initial_value: 'test'
-            },
-            label: {
-              type: 'plain_text',
-              text: 'Message Text'
-            }
-          },
-          {
-            block_id: 'importance',
-            type: 'input',
-            element: {
-              action_id: 'importance_id',
-              type: 'static_select',
-              placeholder: {
-                type: 'plain_text',
-                text: 'Select importance',
-                emoji: true
-              },
-              options: [
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: 'High 💎💎✨',
-                    emoji: true
-                  },
-                  value: 'high'
-                },
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: 'Medium 💎',
-                    emoji: true
-                  },
-                  value: 'medium'
-                },
-                {
-                  text: {
-                    type: 'plain_text',
-                    text: 'Low ⚪️',
-                    emoji: true
-                  },
-                  value: 'low'
-                }
-              ]
-            },
-            label: {
-              type: 'plain_text',
-              text: 'Importance'
-            }
-          }
-        ]
-      })
-    };
-
-    await context.sendActivity(viewData);
-
-    // axios.post('https://slack.com/api/views.open', qs.stringify(viewData))
-    // .then((result) => {
-    //   res.sendStatus(200);
-    // })
-    // .catch((err) => {
-    //   console.log(err);
-    // });
+    // await context.sendActivity(DataView);
   });
 });
 
