4dcb68af56e56036a2fbce2b3a169b06a230fd0b
Hiep Nguyen <hiepnh610@gmail.com>
2020-05-24 16:27:54 +0700

    update code

--- index.js
+++ index.js
@@ -3,84 +3,19 @@ const app         = express();
 const http        = require('http').Server(app);
 const bodyParser  = require('body-parser');
 
-const { SlackAdapter }   = require('botbuilder-adapter-slack');
+const { SlackAdapter, SlackDialog, SlackBotWorker }   = require('botbuilder-adapter-slack');
 
 const CONSTANTS = require('./constants');
 
-const slackAdapter = new SlackAdapter(CONSTANTS.SLACK_APP_OPTIONS);
+const botAdapter = new SlackAdapter(CONSTANTS.SLACK_APP_OPTIONS);
+const botDialog = new SlackDialog('My Dialog', 'callback_123', 'Save');
+const bot = new SlackBotWorker();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/api/messages', (req, res) => {
-  slackAdapter.processActivity(req, res, async(context) => {
-    console.log(context);
-    // const viewData = {
-    //   token: process.env.SLACK_BOT_TOKEN,
-    //   trigger_id: context._activity.channelData.trigger_id,
-    //   view: JSON.stringify({
-    //     "type": "modal",
-    //     "title": {
-    //       "type": "plain_text",
-    //       "text": "My App",
-    //       "emoji": true
-    //     },
-    //     "submit": {
-    //       "type": "plain_text",
-    //       "text": "Submit",
-    //       "emoji": true
-    //     },
-    //     "close": {
-    //       "type": "plain_text",
-    //       "text": "Cancel",
-    //       "emoji": true
-    //     },
-    //     "blocks": [
-    //       {
-    //         "type": "input",
-    //         "element": {
-    //           "type": "plain_text_input",
-    //           "action_id": "sl_input",
-    //           "placeholder": {
-    //             "type": "plain_text",
-    //             "text": "Placeholder text for single-line input"
-    //           }
-    //         },
-    //         "label": {
-    //           "type": "plain_text",
-    //           "text": "Label"
-    //         },
-    //         "hint": {
-    //           "type": "plain_text",
-    //           "text": "Hint text"
-    //         }
-    //       },
-    //       {
-    //         "type": "input",
-    //         "element": {
-    //           "type": "plain_text_input",
-    //           "action_id": "ml_input",
-    //           "multiline": true,
-    //           "placeholder": {
-    //             "type": "plain_text",
-    //             "text": "Placeholder text for multi-line input"
-    //           }
-    //         },
-    //         "label": {
-    //           "type": "plain_text",
-    //           "text": "Label"
-    //         },
-    //         "hint": {
-    //           "type": "plain_text",
-    //           "text": "Hint text"
-    //         }
-    //       }
-    //     ]
-    //   })
-    // };
-
-    // await context.sendActivity(DataView);
-  });
+app.post('/api/shortcut', (req, res) => {
+  botAdapter.processActivity(req, res, async(context) => {});
 });
 
 http.listen(CONSTANTS.PORT, () => {
