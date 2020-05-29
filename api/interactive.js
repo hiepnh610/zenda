03ca8acb68eb88fb0656e92ffac9c3395abcb502
Hiep Nguyen <hiepnh610@gmail.com>
2020-05-29 23:44:05 +0700

    handle send message to channel

--- api/interactive.js
+++ api/interactive.js
@@ -31,8 +31,6 @@ const showModal = (req, res) => {
     }
 
     if (activityType === CONSTANTS.VIEW_SUBMISSION) {
-      res.status(200).end();
-
       const slackView = payload.view;
 
       if (slackView) {
@@ -41,6 +39,8 @@ const showModal = (req, res) => {
 
         switch(radioButtonValue) {
           case CONSTANTS.RADIO_BUTTONS.GIVE:
+            res.status(200).end();
+
             modal.view = giveTemplate;
 
             web.views.open(modal);
@@ -48,6 +48,8 @@ const showModal = (req, res) => {
             break;
 
           case CONSTANTS.RADIO_BUTTONS.GIFT_REQUEST:
+            res.status(200).end();
+
             modal.view = giftRequest
 
             web.views.open(modal);
@@ -55,6 +57,7 @@ const showModal = (req, res) => {
             break;
 
           default:
+            res.status(400);
         }
       }
     }
@@ -63,92 +66,75 @@ const showModal = (req, res) => {
   }
 };
 
-const handleFormData = async (context) => {
-  const channelData = await context._activity.channelData;
-  const slackView = channelData.view;
+const handleDataSubmit = (req) => {
+  if (req && req.body && req.body.payload) {
+    const payload = JSON.parse(req.body.payload);
 
-  if (slackView) {
-    const cbId = slackView.callback_id;
-    const slackValues = slackView.state.values;
+    const slackView = payload.view;
 
-    switch(cbId) {
-      case CONSTANTS.MODAL_CALLBACK.GIVE:
-        giveTheGift(await context);
+    if (slackView) {
+      const cbId = slackView.callback_id;
+      const slackValues = slackView.state.values;
 
-        break;
+      switch(cbId) {
+        case CONSTANTS.MODAL_CALLBACK.GIVE:
+          giveTheGift(req);
 
-      case CONSTANTS.MODAL_CALLBACK.GIFT_REQUEST:
-        console.log(slackValues);
+          break;
 
-        break;
-    }
-  }
-};
+        case CONSTANTS.MODAL_CALLBACK.GIFT_REQUEST:
+          console.log(slackValues);
 
-const giveTheGift = async (context) => {
-  const channelData = context._activity.channelData;
-  const slackView = channelData.view;
-  const fromUser = channelData.user;
-  const slackValues = slackView.state.values;
-  const selectedUser = UTILS.findValue(slackValues, 'selected_user');
-  const quantityValue = parseInt(UTILS.findValue(slackValues, 'value'));
-  const isIntNumber = Number.isInteger(quantityValue);
-  const [
-    fromUserInfo,
-    toUserInfo
-  ] = await getSlacksUserInfo(await context, fromUser.id, selectedUser);
-  const getFromUserInfo = await getOrCreate(fromUserInfo.user);
-
-  const bagIsValid = await checkBag(getFromUserInfo, quantityValue);
-
-  getOrCreate(toUserInfo.user);
-
-  if (bagIsValid && isIntNumber) {
-    const userUpdated = await updateUser(
-      fromUserInfo.user.id,
-      toUserInfo.user.id,
-      quantityValue
-    );
-
-    if (userUpdated) {
-      const payload = {
-        from_user_id: fromUserInfo.user.id,
-        quantity: quantityValue,
-        to_user_id: toUserInfo.user.id
-      };
-
-      const transCreated = await createTransaction(payload);
-
-      if (transCreated) {
-        console.log(await context);
+          break;
       }
     }
   }
 };
 
-const getSlacksUserInfo = async (context, fromUserId, toUserId) => {
-  return await Promise.all([
-    getSlackUserInfo(context, fromUserId),
-    getSlackUserInfo(context, toUserId)
-  ]);
-};
+const giveTheGift = async (req) => {
+  if (req && req.body && req.body.payload) {
+    const payload = JSON.parse(req.body.payload);
+    const slackView = payload.view;
 
-const checkBag = async (fromUserInfo, quantity) => {
-  const giveBag = fromUserInfo.give_bag;
+    const userRequest = payload.user;
+    const userIdRequest = userRequest.id;
 
-  if (giveBag > 0 && giveBag >= quantity) {
-    return true;
+    const valuesRequest = slackView.state.values;
+
+    const userIdReceive = UTILS.findValue(valuesRequest, 'selected_user');
+    const valueQuantity = parseInt(UTILS.findValue(valuesRequest, 'value'));
+    const isIntNumber = Number.isInteger(valueQuantity);
+
+    const getUserRequestInfo = await getOrCreate(userIdRequest);
+    const checkUserRequestBag = checkBag(getUserRequestInfo, valueQuantity);
+
+    if (!checkUserRequestBag) {
+      /* TODO */
+    }
+
+    if (checkUserRequestBag && isIntNumber) {
+      const transactionData = await updateUser(
+        userIdRequest,
+        userIdReceive,
+        valueQuantity
+      );
+
+      if (transactionData) {
+        createTransaction(transactionData);
+      }
+    }
   }
 };
 
-const getSlackUserInfo = (context, userId) => {
-  return context._adapter.slack.users.info({
-    user: userId
-  });
+const checkBag = (userInfo, quantity) => {
+  const giveBag = userInfo.give_bag;
+
+  return (giveBag > 0 && giveBag >= quantity);
 };
 
 const handleInteractiveFromSlack = (req, res) => {
   showModal(req, res);
+  handleDataSubmit(req);
 };
 
 module.exports = {
--- api/transactions.js
+++ api/transactions.js
@@ -1,9 +1,72 @@
+const { WebClient } = require('@slack/web-api');
+
 const Transaction = require('../models/transactions');
+const CONSTANTS = require('../constants');
 
-const createTransaction = async (payload) => {
-  const transaction = new Transaction(payload);
+const web = new WebClient(CONSTANTS.SLACK_APP_OPTIONS.botToken);
 
-  return await transaction.save();
+const createTransaction = (transactionData) => {
+  const transaction = new Transaction(transactionData);
+
+  transaction.save((e, trans) => {
+    if (!e && trans) {
+      sendMessage(transactionData);
+    }
+  });
+};
+
+const getChannelInfo = async () => {
+  const channelList = await web.conversations.list();
+
+  return channelList.channels.filter(channel => channel.name === 'bimbim');
+};
+
+const sendMessage = async (transactionData) => {
+  const targetChannel = await getChannelInfo();
+
+  if (targetChannel.length) {
+    const channelId = targetChannel[0].id;
+    const userRequestId = transactionData.user_request_id;
+    const userReceiveId = transactionData.user_receive_id;
+    const quantity = transactionData.quantity;
+
+    const params = {
+      channel: channelId,
+      text: `<@${userRequestId}> đã gửi tặng <@${userReceiveId}> ${quantity} bimbim.`
+    };
+
+    const isMessageSendSuccess = await web.chat.postMessage(params);
+
+    if (isMessageSendSuccess.ok) {
+      checkUserInChannel(channelId, userRequestId);
+      checkUserInChannel(channelId, userReceiveId);
+    }
+  }
+};
+
+const checkUserInChannel = async (channelId, userId) => {
+  const usersInChannel = await web.conversations.members({
+    channel: channelId
+  });
+
+  if (usersInChannel.ok && usersInChannel.members.length) {
+    const isUserInChannel = await usersInChannel.members.find(member => {
+      return member === userId;
+    });
+
+    if (!isUserInChannel) {
+      inviteUserToChannel(channelId, userId);
+    }
+  }
+};
+
+const inviteUserToChannel = (channelId, userId) => {
+  const params = {
+    channel: channelId,
+    users: userId
+  };
+
+  web.conversations.invite(params);
 };
 
 module.exports = {
