5624b5d8a1ad9ec99782a886320fea1d78b512b4
Hiep Nguyen <hiepnh610@gmail.com>
2020-05-31 23:21:59 +0700

    add cors lib

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express     = require('express');
 const app         = express();
+const cors        = require('cors');
 const http        = require('http').Server(app);
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
@@ -13,6 +14,7 @@ mongoose.connect(
   CONSTANTS.MONGODB_OPTIONS
 );
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
