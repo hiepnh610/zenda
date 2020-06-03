6a28489f4b9c0b9b2b48942f61f906ae6f3d8eb5
Hiep Nguyen <hiepnh610@gmail.com>
2020-06-03 07:50:52 +0700

    update quantity reset api

--- api/settings.js
+++ api/settings.js
@@ -1,18 +1,22 @@
 const User = require('../models/users');
 
 const resetPoints = (req, res) => {
-  User.updateMany(
-    {},
-    {
-      $set: { give_bag: 10, receive_bag: 0 },
-    },
-    { multi: true },
-    (e) => {
-      if (e) return res.status(400).json(e);
+  const quantity = req.body.quantity;
 
-      res.status(200).json({ message: 'Done' });
-    }
-  );
+  if (req.body.quantity) {
+    User.updateMany(
+      {},
+      {
+        $set: { give_bag: quantity, receive_bag: 0 },
+      },
+      { multi: true },
+      (e) => {
+        if (e) return res.status(400).json(e);
+
+        res.status(200).json({ message: 'Done' });
+      }
+    );
+  }
 };
 
 module.exports = {
